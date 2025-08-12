ca3edaea3dbdd24dfac8d2e8e4910f06435fdd86
MartuZeman <marty-z97@live.com>
2025-08-12 19:51:44 -0300

    Vaciar form + modo oscuro

--- main.js
+++ main.js
@@ -68,7 +68,11 @@ const nombreMedico = document.getElementById("nombre-medico")
 const especialidadMedico = document.getElementById("especialidad-medico")
 const horariosMedico = document.getElementById("horarios-medico")
 const imgMedico = document.getElementById("foto-medico")
+const camposFormulario = document.querySelectorAll("#form-medico input")
 
+const btnModoOscuro = document.getElementById("btnModoOscuro");
+const body = document.body;
+const temaGuardado = localStorage.getItem("tema");
 
 //Guardar medicos iniciales
 
@@ -129,10 +133,27 @@ function nuevoMedico(n,e,h,img)
 
 }
 
-formulario.addEventListener("submit", (evt) =>{
-    evt.preventDefault();
+function limpiarCampos(campoFormulario){
 
-    alert(medicos.length)
+    campoFormulario.forEach((campos) => {
+        campos.value = ""
+    })
+
+}
+
+formulario.addEventListener("submit", (evt) =>{
+       evt.preventDefault(); 
+    if(nombreMedico.value === ""){
+        alert("Debe completar el nombre del medico")
+    }else if(especialidadMedico.value === "") {
+        alert("Debe completar la especialidad del medico")
+    }else if(horariosMedico.value === ""){
+        alert("Debe completar el horario del medico")
+    }else if(imgMedico.value === ""){
+        alert("Debe cargar la imagen del medico")
+    }else{ 
+
+
     n = nombreMedico.value
     e = especialidadMedico.value
     h = horariosMedico.value
@@ -140,8 +161,34 @@ formulario.addEventListener("submit", (evt) =>{
 
     medicos.push(new nuevoMedico(n,e,h,img))
 
+    console.log(camposFormulario)
+
     guardarMedicos()
     actualizarCarrusel()
+    limpiarCampos(camposFormulario)
+    }
 
 })
+
+//modo oscuro
+
+btnModoOscuro.addEventListener("click", () => {
+
+ body.classList.toggle("dark-mode")
+
+   if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("tema", "dark");
+  } else {
+    localStorage.setItem("tema", "light");
+  }
+
+
+})
+
+if (temaGuardado === "dark") {
+  body.classList.add("dark-mode");
+} else {
+  // Asegurarse de que no haya un modo oscuro residual si la preferencia es light
+  body.classList.remove("dark-mode");
+}
 
